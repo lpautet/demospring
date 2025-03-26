1deda2ff98f82e3ab49d191388eac3b069bdf2cf
Laurent Pautet <lpautet@salesforce.com>
2025-03-26 18:38:36 +0100

    Managing token refresh...

--- src/main/java/net/pautet/softs/demospring/config/AppConfig.java
+++ src/main/java/net/pautet/softs/demospring/config/AppConfig.java
@@ -1,13 +1,24 @@
 package net.pautet.softs.demospring.config;
 
 import lombok.Data;
+import org.springframework.boot.autoconfigure.data.redis.LettuceClientConfigurationBuilderCustomizer;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Data
 @ConfigurationProperties("app")
 @Configuration
 public class AppConfig {
+
+    @Bean
+    public LettuceClientConfigurationBuilderCustomizer lettuceClientConfigurationBuilderCustomizer() {
+        return clientConfigurationBuilder -> {
+            if (clientConfigurationBuilder.build().isUseSsl()) {
+                clientConfigurationBuilder.useSsl().disablePeerVerification();
+            }
+        };
+    }
 
     private String redirectUri;
     private String jwtSecret;
